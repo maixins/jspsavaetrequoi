d688ac8182fbc97e7307a58ea44a71950bd8268b
maixins <130320629+maixins@users.noreply.github.com>
2023-06-05 20:59:20 +0200

    Add files via upload

--- database/database.py
+++ database/database.py
@@ -2,8 +2,8 @@ from tinydb import TinyDB, Query
 from tinydb.operations import add
 from datetime import datetime
 
-db_account = TinyDB('db_account.json')
-db_message = TinyDB("db_message.json")
+db_account = TinyDB('database/db_account.json')
+db_message = TinyDB("database/db_message.json")
 user = Query()
 
 
@@ -11,7 +11,7 @@ def register(name, mdp):
     result = db_account.search(user.name == name)
     if not len(result) > 0:
         if name[0].isalpha():
-            db_account.insert({"name": name, "mdp": str(mdp), "message": [], "pdp_filename": "default"})
+            db_account.insert({"name": name, "mdp": str(mdp), "message": [], "pdp_filename": "default.png"})
             return 0,f"Bienvenue {name}!"
         else:
             return 1,f"{name} n'est pas valide, il faut que le premier caractère soit une lettre"
@@ -28,15 +28,29 @@ def login(name,mdp):
         if result[0]["mdp"] == mdp:
             return 0,f"Heureux de vous revoir {name}"
         else:
-            return 1,f"Je crains que vous vous êtes trompés de mot de passe{name.capitalize()}"
+            return 1,f"Je crains que vous vous êtes trompés de mot de passe"
 
 
-def send(name,message):
+def send(name, message):
     now = datetime.now()
-    db_account.update(add("message", [{"message":message,"date":now.strftime("%d/%m/%Y %H:%M:%S")}]), user.name == name)
-    db_message.insert({"message":message,"date":now.strftime("%d/%m/%Y %H:%M:%S"),"name":name})
+    db_account.update(add("message", [{"message":message, "date":now.strftime("%d/%m/%Y %H:%M:%S")}]), user.name == name)
+    db_message.insert({"message": message, "date": now.strftime("%d/%m/%Y %H:%M:%S"),'profile_pic':pdp_of(name) ,"name": name})
+
 
 
 def message_from(name):
-    return db_account.search(user.name == name)[0]["message"]
+    return db_message.search(user.name.all(name))
+
+
+def pdp_of(name):
+  return db_account.search(user.name == name)[0]["pdp_filename"]
+
+def update_profile_picture(username, image_filename):
+    db_account.update({'pdp_filename': image_filename}, user.name == username)
+
+
+def get_last_messages():
+    messages = db_message.all()[-25:]
+    return messages
+
 
